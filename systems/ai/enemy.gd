dcce4749b364cb763a88a85d455750708c1ad445
Gavin Wilson <68349014+EyeAmTheOne@users.noreply.github.com>
2025-11-04 15:09:07 -0800

    Simple enemy move towards player

---
+++ systems/ai/enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+@onready var p = $"../Player";
+
+const SPEED = 10
+
+var target = null
+
+func set_target(player: Node2D):
+	target = player;
+
+func _physics_process(delta: float) -> void:
+	# If target exists, move towards target
+	var direction = Vector2(0, 0);
+	if p:
+		set_target(p);
+	if target:
+		direction = position.direction_to(target.position);
+		
+	velocity = direction * SPEED;
+	move_and_slide();
