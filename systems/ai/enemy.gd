bec122b6820743a74e4ec46173db3f9dbf5eeb85
Gavin Wilson <68349014+EyeAmTheOne@users.noreply.github.com>
2025-11-18 18:52:21 -0800

    Add enemy walk animation and switch both player and enemy to "floating" instead of "grounded" mode
    Grounded mode is meant for 2d sidecrolling games rather than for
    top-down games

--- systems/ai/enemy.gd
+++ systems/ai/enemy.gd
@@ -1,24 +1,39 @@
 extends CharacterBody2D
-@onready var p = $"../Player";
+@onready var animated_sprite = $AnimatedSprite2D
 
-
-
-const SPEED = 10
+const SPEED = 50
 
 var target = null
 
 func set_target(player: Node2D):
-	target = player;
+	target = player
+	
+func get_orientation():
+	if velocity[0] > 0:
+		# Look left
+		animated_sprite.flip_h = false
+	elif velocity[0] < 0:
+		# Look right
+		animated_sprite.flip_h = true
 
 func _physics_process(delta: float) -> void:
 	# If target exists, move towards target
-	var direction = Vector2(0, 0);
+	var direction = Vector2(0, 0)
 	if target:
-		direction = position.direction_to(target.position);
+		animated_sprite.play("Walk")
+		direction = position.direction_to(target.position)
+	else:
+		animated_sprite.play("Idle")
 		
-	velocity = direction * SPEED;
-	move_and_slide();
+	velocity = direction * SPEED
+	get_orientation()
+	move_and_slide()
 
 
 func _on_detection_body_entered(body: Node2D) -> void:
-	set_target(body);
+	set_target(body)
+	
+
+func _on_detection_body_exited(body: Node2D) -> void:
+	if body == target:
+		target = null
