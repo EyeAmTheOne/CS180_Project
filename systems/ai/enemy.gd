c4dd15bd390898b8b95dbdd7941af041d6d430e9
Gavin Wilson <68349014+EyeAmTheOne@users.noreply.github.com>
2025-11-18 18:29:59 -0800

    Add detection area for enemy follow player

--- systems/ai/enemy.gd
+++ systems/ai/enemy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 @onready var p = $"../Player";
 
+
+
 const SPEED = 10
 
 var target = null
@@ -11,10 +13,12 @@ func set_target(player: Node2D):
 func _physics_process(delta: float) -> void:
 	# If target exists, move towards target
 	var direction = Vector2(0, 0);
-	if p:
-		set_target(p);
 	if target:
 		direction = position.direction_to(target.position);
 		
 	velocity = direction * SPEED;
 	move_and_slide();
+
+
+func _on_detection_body_entered(body: Node2D) -> void:
+	set_target(body);
