fa63c159ef122d1f8472639364e66c7ef1fbcbc4
Gavin Wilson <68349014+EyeAmTheOne@users.noreply.github.com>
2025-11-19 08:15:55 -0800

    Hitbox correctly flips to opposite side when enemy turns around, and activates only on the swing of the attack animation

--- systems/ai/enemy.gd
+++ systems/ai/enemy.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
+@onready var character = $"."
+@onready var hitbox = $HitBox/CollisionShape2D
 
 const SPEED = 50
 
@@ -12,19 +15,25 @@ func get_orientation():
 	if velocity[0] > 0:
 		# Look left
 		animated_sprite.flip_h = false
+		#Flip weapon hitbox to be on left side
+		hitbox.position[0] = abs(hitbox.position[0])
+
 	elif velocity[0] < 0:
 		# Look right
 		animated_sprite.flip_h = true
+		# Flip weapon hitbox to be on right side
+		hitbox.position[0] = -abs(hitbox.position[0])
+
 
 func _physics_process(delta: float) -> void:
 	# If target exists, move towards target
 	var direction = Vector2(0, 0)
 	if target:
-		if position.distance_to(target.position) > 10:
+		if position.distance_to(target.position) > 20:
 			animated_sprite.play("Walk")
 			direction = position.direction_to(target.position)
 		else:
-			animated_sprite.play("Attack")
+			animation_player.play("Attack")
 	else:
 		animated_sprite.play("Idle")
 		
