ee4102b0d68e89505b00b974993f6cdc174bb65b
diego32908 <ddelg052@ucr.edu>
2025-11-22 22:36:20 -0800

    Resolve merge + restore interact input

--- player/player.gd
+++ player/player.gd
@@ -10,6 +10,14 @@ var player_dead : bool = false
 var current_attack : bool = false
 var stun : bool = false
 
+# Diego added
+var wood: int = 0
+var coal: int = 0
+var gold: int = 0
+var metal: int = 0
+#### Diego code ends here
+
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
@@ -19,20 +27,21 @@ func get_orientation():
 	if velocity[0] > 0:
 		# Look left
 		animated_sprite.flip_h = false
-		#Flip weapon hitbox to the left
+		# Flip weapon hitbox to the left
 		hitbox.position[0] = abs(hitbox.position[0])
 	elif velocity[0] < 0:
 		# Look right
 		animated_sprite.flip_h = true
-		#Flip weapon hitbox to the right
+		# Flip weapon hitbox to the right
 		hitbox.position[0] = -abs(hitbox.position[0])
-		
+
 
 func _physics_process(delta):
 	if player_active:
 		get_input()
 		get_orientation()
 
+		# attacks
 		if !current_attack and !stun:
 			if Input.is_action_just_pressed("attack"):
 				current_attack = true
@@ -40,15 +49,46 @@ func _physics_process(delta):
 
 		move_and_slide()
 
+		# walk / idle anim
 		if !current_attack and !stun:
 			if !Input.get_vector("left", "right", "up", "down").is_zero_approx():
 				animated_sprite.play("walk")
 			else:
 				animated_sprite.play("idle")
+
+		# Diego: interact with resources
+		if Input.is_action_just_pressed("interact"):
+			_try_interact()
 	else:
 		if !player_dead:
 			animated_sprite.play("death")
 			player_dead = true
+
+
+# Diego added ↓
+func add_wood(amount: int) -> void:
+	wood += amount
+	print("Picked up ", amount, " wood (Total: ", wood, ")")
+	_show_pickup_popup("+%d wood" % amount)
+
+
+func add_coal(amount: int) -> void:
+	coal += amount
+	print("Picked up ", amount, " coal (Total: ", coal, ")")
+	_show_pickup_popup("+%d coal" % amount)
+
+
+func add_gold(amount: int) -> void:
+	gold += amount
+	print("Picked up ", amount, " gold (Total: ", gold, ")")
+	_show_pickup_popup("+%d gold" % amount)
+
+
+func add_metal(amount: int) -> void:
+	metal += amount
+	print("Picked up ", amount, " metal (Total: ", metal, ")")
+	_show_pickup_popup("+%d metal" % amount)
+#### Diego code ends here
 
 
 func attack_animation():
@@ -62,8 +102,18 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	elif anim_name == "Hurt":
 		stun = false
 		current_attack = false
-	
-	
+
+
+# Diego added ↓
+func _try_interact() -> void:
+	for area in $InteractArea.get_overlapping_areas():
+		var target = area.get_parent()
+		if target.has_method("harvest"):
+			target.harvest(self)
+			return
+#### Diego code ends here
+
+
 func _on_player_health_health_depleted() -> void:
 	player_active = false
 	print("YOU HAVE DIED")
@@ -73,3 +123,12 @@ func _on_hurt_box_received_damage(damage: int) -> void:
 	if player_active:
 		stun = true
 		animated_player.play("Hurt")
+
+
+### new function
+# Diego added (shows pick up item)
+func _show_pickup_popup(text: String) -> void:
+	var ui = get_tree().get_first_node_in_group("ui")
+	if ui:
+		ui.show_pickup(text)
+#### Diego code ends here
