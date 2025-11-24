6bdd853fe80121eede370c187f4ecfbf7fab80db
Gavin Wilson <68349014+EyeAmTheOne@users.noreply.github.com>
2025-11-23 17:16:36 -0800

    Make camera properly switch when entering a new scene

--- world/world.gd
+++ world/world.gd
@@ -1,5 +1,8 @@
 extends BaseScene
 
+@onready var camera: Camera2D = $PlayerCamera
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
+	camera.follow_node = player
